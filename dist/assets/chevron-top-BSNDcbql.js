f5c1ed1c216105e57af8493fb3db8a09d2ea7282
s4nyx <sanyscott4139@gmail.com>
2025-08-25 21:47:00 +0900

    release 0.1.1 images optimize

--- dist/assets/chevron-top-BSNDcbql.js
+++
@@ -1,8 +0,0 @@
-import{b as o}from"./core-BJS_zDgY.js";import"./index-D2NprVaW.js";import"./events-Cmms7cPu.js";import"./index.es-SR7Z9FCI.js";import"./index-nibyPLVP.js";const t=o`<svg fill="none" viewBox="0 0 16 16">
-  <path
-    fill="currentColor"
-    fill-rule="evenodd"
-    d="M14.54 11.04a1 1 0 0 1-1.41 0L8 5.92l-5.13 5.12a1 1 0 1 1-1.41-1.41l5.83-5.84a1 1 0 0 1 1.42 0l5.83 5.84a1 1 0 0 1 0 1.41Z"
-    clip-rule="evenodd"
-  />
-</svg>`;export{t as chevronTopSvg};
